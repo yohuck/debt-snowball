8120702cc7c5a8c497dc9043dee173732467e419
yohuck <ericallenlake@gmail.com>
2023-12-02 13:29:17 -0700

    SnowballProcessor Class created & referenced

---
+++ debt-snowball/Utilities/SnowballProcessor.cs
@@ -0,0 +1,20 @@
+﻿namespace debt_snowball.Utilities
+{
+    public class SnowballProcessor
+{
+    
+        public static void ProcessSnowball(double extraPayment, List<Debt> debts)
+        {
+            Console.WriteLine(extraPayment);
+            foreach (Debt debt in debts)
+            {
+                Console.WriteLine(debt.Name);
+                Console.WriteLine(debt.Balance);
+                Console.WriteLine(debt.MinimumPayment);
+                Console.WriteLine(debt.Rate);
+                Console.WriteLine(debt.DueDay);
+
+            }
+        }
+    }
+}
